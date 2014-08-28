a0e2b35790403a1826ffa8d3e61cdccf6ab9911f
henen254 <henen254@gmail.com>
2014-08-28 20:41:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-Test
-
-<?php
-
-echo(phpinfo());
-
-?>
+<iframe id="ytplayer" type="text/html" width="640" height="360"
+src="https://www.youtube.com/embed/3u8Tbzj37cc"
+frameborder="0" allowfullscreen>
