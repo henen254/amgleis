2d5ee89a64c08ce1b2aff11fa46c78f9e1b23b5a
henen254 <henen254@gmail.com>
2014-08-30 22:06:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <head>
 </head>
 <body>
-<p><iframe id="ytplayer" type="text/html" width="640" height="360"
+<p><iframe id="player" type="text/html" width="640" height="360"
 src="https://www.youtube.com/embed/3u8Tbzj37cc?version=3&enablejsapi=1"
 frameborder="0" allowfullscreen></iframe>
 
 <p><form>
-<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+<button type="butonplay" onclick="player.playVideo():Void">Start</button>
+<button type="buttonpause" onclick="player.pauseVideo():Void">Pause</button>
 
 </form>
 </body>
