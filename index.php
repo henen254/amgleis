8575c7c51f5f1133ec0cd9e8e9ffc3e01cb8e96a
henen254 <henen254@gmail.com>
2014-08-30 22:12:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
 src="https://www.youtube.com/embed/3u8Tbzj37cc?version=3&enablejsapi=1"
 frameborder="0" allowfullscreen></iframe>
 
+<script type="text/javascript">
+
+player = document.getElementById("ytplayer");
+</script>
+
 <p><form>
-<button type="butonplay" onclick="player.playVideo():Void">Start</button>
-<button type="buttonpause" onclick="player.pauseVideo():Void">Pause</button>
+<button type="butonplay" onclick="player.playVideo();">Start</button>
+<button type="buttonpause" onclick="player.pauseVideo();">Pause</button>
 
 </form>
 </body>
