904d336255f104ba44b38f659dc495cbcf574b5a
henen254 <henen254@gmail.com>
2014-08-30 19:46:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <iframe id="ytplayer" type="text/html" width="640" height="360"
-src="https://www.youtube.com/embed/3u8Tbzj37cc"
+src="https://www.youtube.com/embed/3u8Tbzj37cc?version=3&enablejsapi=1"
 frameborder="0" allowfullscreen>
+
+<button type="butten" onClick="alert('test);">pause</button>
