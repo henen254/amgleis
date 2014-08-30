a3ef406d258b43da793e9077d9e7b480f42e3c81
henen254 <henen254@gmail.com>
2014-08-30 21:59:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 <p><iframe id="ytplayer" type="text/html" width="640" height="360"
 src="https://www.youtube.com/embed/3u8Tbzj37cc?version=3&enablejsapi=1"
-frameborder="0" allowfullscreen>
+frameborder="0" allowfullscreen></iframe>
 
 <p><form>
 <button type="button" onclick="alert('Hello world!')">Click Me!</button>
