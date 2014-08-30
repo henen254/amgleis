2dfe0793ee33573a834e5ac42b4d8b287e7cde91
henen254 <henen254@gmail.com>
2014-08-30 22:14:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-<p><iframe id="player" type="text/html" width="640" height="360"
+<p><iframe id="ytplayer" type="text/html" width="640" height="360"
 src="https://www.youtube.com/embed/3u8Tbzj37cc?version=3&enablejsapi=1"
 frameborder="0" allowfullscreen></iframe>
 
